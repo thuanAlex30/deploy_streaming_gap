bd9ad3f8e3f743e825932042a14968692001208a
thuanAlex30 <thuandh30@gmail.com>
2024-10-04 10:34:19 +0700

    delete new folder

--- New folder/src/main/java/com/fpt/StreamGAP/repository/SongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fpt.StreamGAP.repository;
-
-import com.fpt.StreamGAP.entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SongRepository extends JpaRepository<Song, Integer> {
-}

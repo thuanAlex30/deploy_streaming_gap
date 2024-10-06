29ddf389c2f18bb7f61dfadd0659a2a0afa4dbf9
thuanAlex30 <thuandh30@gmail.com>
2024-10-06 11:00:48 +0700

    Updated MusicGame entity, UserRepo, and application.properties

--- src/main/java/com/fpt/StreamGAP/repository/UserRepo.java
+++ src/main/java/com/fpt/StreamGAP/repository/UserRepo.java
@@ -3,10 +3,11 @@ package com.fpt.StreamGAP.repository;
 import com.fpt.StreamGAP.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User,Integer> {
 
-
+    List<User> findByRole(String role);
     Optional<User> findByEmail(String email);
 }
